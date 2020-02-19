06dd5ad13c707213a4663606b94dc2d87ecf8888
Oleg Vaiser <aly9681082@gmail.com>
2020-02-19 17:28:07 +0200

    first commit

---
+++ src/main/java/com/example/speaking/SpeakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.speaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakingApplication.class, args);
+    }
+
+}
