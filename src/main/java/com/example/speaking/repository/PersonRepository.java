49b17111546ec2ee9258ececd89359ff92b58851
Oleg Vaiser <aly9681082@gmail.com>
2020-03-01 18:05:45 +0200

    correct

--- src/main/java/com/example/speaking/repository/PersonRepository.java
+++ src/main/java/com/example/speaking/repository/PersonRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PersonRepository extends MongoRepository <Person,String> {
 
-
+    Person findByEmailAndPassword(String email,String password);
 }
--- src/main/java/com/example/speaking/service/impl/PersonServiceImpl.java
+++ src/main/java/com/example/speaking/service/impl/PersonServiceImpl.java
@@ -1,20 +1,30 @@
 package com.example.speaking.service.impl;
 
+import com.example.speaking.exception.AuthenticationException;
 import com.example.speaking.model.dto.person.login.LoginRequest;
 import com.example.speaking.model.dto.person.login.LoginResponse;
 import com.example.speaking.model.dto.person.register.RegisterRequest;
 import com.example.speaking.model.dto.person.register.RegisterResponse;
 import com.example.speaking.model.entity.Person;
+import com.example.speaking.model.entity.PersonSession;
 import com.example.speaking.repository.PersonRepository;
+import com.example.speaking.repository.PersonSessionRepository;
 import com.example.speaking.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Slf4j
 @Service
 public class PersonServiceImpl implements PersonService {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @Autowired
+    private PersonSessionRepository personSessionRepository;
 
     @Override
     public RegisterResponse savePerson(RegisterRequest registerRequest) {
@@ -40,7 +50,25 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        return null;
+
+        Person person = personRepository.findByEmailAndPassword
+                (loginRequest.getEmail(), loginRequest.getPassword());
+        if (person == null) {
+            log.warn("Incorrect username or password. Incoming parameters are: {} - {}",
+                    loginRequest.getEmail(), loginRequest.getPassword());
+            throw new AuthenticationException("Username or password is incorrect");
+        }
+        PersonSession personSession =
+                personSessionRepository.save(PersonSession.builder()
+                        .sessionId(UUID.randomUUID().toString())
+                        .person(person)
+                        .isValid(true)
+                .build());
+        log.debug("User with name = {} logged in", person.getFirstName());
+
+        return LoginResponse.builder()
+                .token(personSession.getSessionId())
+                .build();
     }
 }
 
