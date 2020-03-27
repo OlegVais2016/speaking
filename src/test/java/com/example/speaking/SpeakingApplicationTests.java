035272b05d94d6121fd88f9c6b61431204263d0a
Oleg Vaiser <aly9681082@gmail.com>
2020-03-27 20:04:10 +0300

    correct

---
+++ src/test/java/com/example/speaking/SpeakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
